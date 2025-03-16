0fdc850b604f273bff95f6812919bfe56cfe6ca6
Pako2425 <patryk.kocinski340@gmail.com>
2025-03-16 21:52:37 +0100

    Category menu buttons created. TODO: Create button-menu-class

--- my-app/src/components/CategoryMenu.jsx
+++ my-app/src/components/CategoryMenu.jsx
@@ -1,15 +1,15 @@
 function CategoryMenu() {
     return(
-        <ul className="flex bg-blue-100 p-5 justify-between">
-            <li><a href="#">Laptopy</a></li>
-            <li><a href="#">Komputery</a></li>
-            <li><a href="#">Tablety</a></li>
-            <li><a href="#">Telefony</a></li>
-            <li><a href="#">Smartwatche</a></li>
-            <li><a href="#">Drukarki</a></li>
-            <li><a href="#">Konsole</a></li>
-            <li><a href="#">RTV</a></li>
-            <li><a href="#">Sieci</a></li>
+        <ul className="flex pl-5 pr-5 h-15 bg-blue-100 items-center justify-center">
+            <li className="hover:bg-white h-full flex items-center p-5"><a href="#">Laptopy</a></li>
+            <li className="hover:bg-white h-full flex items-center p-5"><a href="#">Komputery</a></li>
+            <li className="hover:bg-white h-full flex items-center p-5"><a href="#">Tablety</a></li>
+            <li className="hover:bg-white h-full flex items-center p-5"><a href="#">Telefony</a></li>
+            <li className="hover:bg-white h-full flex items-center p-5"><a href="#">Smartwatche</a></li>
+            <li className="hover:bg-white h-full flex items-center p-5"><a href="#">Drukarki</a></li>
+            <li className="hover:bg-white h-full flex items-center p-5"><a href="#">Konsole</a></li>
+            <li className="hover:bg-white h-full flex items-center p-5"><a href="#">RTV</a></li>
+            <li className="hover:bg-white h-full flex items-center p-5"><a href="#">Sieci</a></li>
         </ul>
     );
 }
