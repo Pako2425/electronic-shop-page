4b5ea466bcd6a0f0df67c8d8216f45b258578472
Pako2425 <patryk.kocinski340@gmail.com>
2025-03-17 13:40:00 +0100

    Firts version of ListOfProducts section

--- my-app/src/components/ListOfProducts.jsx
+++ my-app/src/components/ListOfProducts.jsx
@@ -1,21 +1,38 @@
 function ListOfProducts() {
     return(
         <div className="produkty">
-            <h2>Produkty</h2>
-            <div class="okazja-dnia">
-                <p>OKAZJA</p>
-            </div>
-            <div class="polecamy">
-                <h2>Polecamy</h2>
-                <ul>
-                    <li>Produkt1</li>
-                    <li>Produkt2</li>
-                    <li>Produkt3</li>
-                    <li>Produkt4</li>
-                    <li>Produkt5</li>
-                    <li>Produkt6</li>
-                    <li>Produkt7</li>
-                </ul>
+            <h2 className="text-5xl font-bold text-center">Produkty</h2>
+            <div className="flex flex-row justify-center items-center">
+                <div className="okazja flex flex-col h-64 bg-amber-200">
+                    <h1 className="text-3xl">OKAZJA</h1>
+                    <img src="./public/newest-products/mouse.png" alt="mouse" className="w-40 object-contain"/>
+                </div>
+                <div class="polecamy flex-col w-2/3 bg-cyan-400">
+                    <h1 className="text-3xl">Polecamy</h1>
+                    <div className="grid grid-cols-3 grid-rows-2 w-12">
+                        <div className="flex justify-center items-center">
+                            <img src="./public/newest-products/laptop.png" alt="product1" className="w-full h-full object-contain"/>
+                        </div>
+                        <div className="flex justify-center items-center">
+                            <img src="./public/newest-products/graphic-card.png" alt="product1" className="w-full h-full object-contain"/>
+                        </div>
+                        <div className="flex justify-center items-center">
+                            <img src="./public/newest-products/keyboard.png" alt="product1" className="w-full h-full object-contain"/>
+                        </div>
+                        <div className="flex justify-center items-center">
+                            <img src="./public/newest-products/monitor.png" alt="product1" className="w-full h-full object-contain"/>
+                        </div>
+                        <div className="flex justify-center items-center">
+                            <img src="./public/newest-products/mouse.png" alt="product1" className="w-full h-full object-contain"/>
+                        </div>
+                        <div className="flex justify-center items-center">
+                            <img src="./public/newest-products/playstation.png" alt="product1" className="w-full h-full object-contain"/>
+                        </div>
+                        <div className="flex justify-center items-center">
+                            <img src="./public/newest-products/printer.png" alt="product1" className="w-full h-full object-contain"/>
+                        </div>
+                    </div>
+                </div>
             </div>
         </div>
     );
--- my-app/src/components/MainAdvertisement.jsx
+++ my-app/src/components/MainAdvertisement.jsx
@@ -1,7 +1,9 @@
 function MainAdvertisement() {
     return(
-        <div className="reklama-sklepowa">
-            <p>Wielkie miejsce na reklamy i aktualności sklepu</p>
+        <div className="reklama-sklepowa flex justify-center items-center h-80">
+            <img src="./public/arrow-left.png" />
+            <img src="./public/main-adv-1.png" alt="reklama" className="h-full m-5"/>
+            <img src="./public/arrow-right.png"/>
         </div>
     );
 }
