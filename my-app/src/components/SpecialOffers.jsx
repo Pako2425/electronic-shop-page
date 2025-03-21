3131c45cd3f8595d08016fccbca369d9aa26650a
Pako2425 <patryk.kocinski340@gmail.com>
2025-03-21 23:02:24 +0100

    Section PROMOCJE v1. TODO: better UI

--- my-app/src/components/SpecialOffers.jsx
+++ my-app/src/components/SpecialOffers.jsx
@@ -1,13 +1,22 @@
 function SpecialOffers() {
     return(
-        <div className="promocje">
+        <div className="promocje flex flex-col justify-center items-center border">
             <h2>Promocje</h2>
-            <ul>
-                <li>Promocja1</li>
-                <li>Promocja2</li>
-                <li>Promocja3</li>
-                <li>Promocja4</li>
-            </ul>
+            <div className="flex w-full max-w-300 border">
+                <div className="flex grow-1 bg-amber-300">
+                    <p>Big offer</p>
+                </div>
+                <div className="flex grow-2 bg-blue-500">
+                    <div className="grid grid-cols-2 grid-rows-2 w-full h-full">
+                        <p1 className="border">a</p1>
+                        <p1 className="border">a</p1>
+                        <p1 className="border">a</p1>
+                        <p1 className="border">a</p1>
+                    </div>
+                </div>
+            </div>
+            
+            
         </div>
     );
 }
