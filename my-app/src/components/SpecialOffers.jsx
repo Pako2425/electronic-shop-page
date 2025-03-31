430d78744a5ab3eab56d2020be908da5706a04f3
Pako2425 <patryk.kocinski340@gmail.com>
2025-03-31 20:43:33 +0200

    Fix special offers layout on home page

--- my-app/src/components/SpecialOffers.jsx
+++ my-app/src/components/SpecialOffers.jsx
@@ -2,14 +2,19 @@ import SpecialOfferCard from "./SpecialOfferCard";
 
 function SpecialOffers() {
     return(
-        <div className="promocje flex flex-col justify-center items-center border">
+        <div className="promocje">
             <div>Promocje</div>
             <p>Sprawdź co dla ciebie mamy</p>
-            <div className="flex gap-4">
-                <SpecialOfferCard img_src={"./public/special-offers/promocja1.png"} title={"#CYBER PONIEDZIAŁEK"} />
-                <SpecialOfferCard img_src={"./public/special-offers/promocja2.png"} title={"2+1"} />
-                <SpecialOfferCard img_src={"./public/special-offers/promocja3.png"} title={"CASHBACK"} />
-                <SpecialOfferCard img_src={"./public/special-offers/promocja4.png"} title={"WYMIEŃ STARE NA NOWE"} />
+            <div className="flex flex-row">
+                <div className="flex flex-1/2 bg-amber-200">
+                    <p>aa</p>
+                </div>
+                <div className="flex w-1/2 gap-4 flex-wrap items-center justify-center">
+                    <SpecialOfferCard img_src={"./public/special-offers/promocja1.png"} title={"#CYBER PONIEDZIAŁEK"} />
+                    <SpecialOfferCard img_src={"./public/special-offers/promocja2.png"} title={"2+1"} />
+                    <SpecialOfferCard img_src={"./public/special-offers/promocja3.png"} title={"CASHBACK"} />
+                    <SpecialOfferCard img_src={"./public/special-offers/promocja4.png"} title={"WYMIEŃ STARE NA NOWE"} />
+                </div>
             </div>
         </div>
     );
